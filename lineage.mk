8162ab6f068b2cf4cfb2edc1a242be80d9685564
Kasual <mushroomhead52e@gmail.com>
2017-01-03 22:02:09 +0300

    d850: rebrand to Lineage
    Change-Id: Iecccdecd15c8f68d2b5840cb33c208223a4b6677

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/lge/d850/full_d850.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
