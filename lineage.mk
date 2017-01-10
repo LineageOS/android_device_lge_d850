5908a6d8b0d980221ad2daed470ecb963a9b02cc
Kasual <mushroomhead52e@gmail.com>
2017-01-10 19:29:05 +0300

    d850: Remove enhanced NFC feature
    * Deprecated
    
    Change-Id: Ib9eaf3aa3e8e02419c3cbf48e3399ec8299dcece

--- lineage.mk
+++ lineage.mk
@@ -3,9 +3,6 @@ $(call inherit-product, device/lge/d850/full_d850.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 PRODUCT_NAME := lineage_d850
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
