a6422012384c5a60e2050d2b7e6578d1f97bff5a
Kasual <mushroomhead52e@gmail.com>
2017-01-19 05:52:44 +0300

    d850: cm->lineage
    Change-Id: I4a2e8670381437f880b271d7ea49c6ff077ba44f

--- lineage.mk
+++ lineage.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/d850/full_d850.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_d850
+PRODUCT_NAME := lineage_d850
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
