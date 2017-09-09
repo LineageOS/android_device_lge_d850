3f1145b0657d19020c779d5c9c8d1eb4adc55d3a
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-08 17:48:17 -0700

    d850: Update for early-mounted system partition
     * This removes /system entry from fstab, since this partition
       is now early-mounted and the fstab entry is specified in device tree.
    
     * At the same time, also include a fully populated fstab, in order to avoid
       build breakage while generating recovery updater scripts.
    
    Change-Id: I8e558458b984e97a45a920c3751a6b7c3f2ebc5c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 27325360128
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/lge/d850/rootdir/etc/fstab.g3
+TARGET_RECOVERY_FSTAB := device/lge/d850/rootdir/etc/fstab.recovery
 
 # NFC
 BOARD_NFC_CHIPSET := pn547
