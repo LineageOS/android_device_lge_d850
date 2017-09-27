2aa30ffe94033bf98b664bd9aafb0795be0ff939
Michael Bestas <mkbestas@lineageos.org>
2017-09-28 02:44:54 +0300

    d850: Cleanup bluetooth config
    * Remove unused flags
    * MAX_L2CAP_CHANNELS defaults to 16
    
    Change-Id: Ide4ecba5eda5fb96d2cf3bf74217334216287cb1

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,20 +17,10 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BLUETOOTH_QTI_SW TRUE
-
 #define BTM_DEF_LOCAL_NAME "LG G3 D850"
 
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_L2CAP_CHANNELS 16
-#define BLE_VND_INCLUDED TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD FALSE
-#define BLE_PERIPHERAL_ADV_NAME FALSE
-#define BT_CLEAN_TURN_ON_DISABLED 1
-
-/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
+#define BLE_VND_INCLUDED          TRUE
+#define BLUETOOTH_QTI_SW          TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 
 #endif
