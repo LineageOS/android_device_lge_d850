e5051e927c7480dded580d8e3ac00682004fccc9
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-08-14 17:52:15 +0200

    d850: Merge full_d850.mk to lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,34 @@
-$(call inherit-product, device/lge/d850/full_d850.mk)
+#
+# Copyright 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from d850 device
+$(call inherit-product, device/lge/d850/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Set those variables here to overwrite the inherited values.
+PRODUCT_DEVICE := d850
 PRODUCT_NAME := lineage_d850
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-D850
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
