ee1d26d3a901fb8e2deec6af59fb5a8cfc7b6de9
Bruno Martins <bgcngm@gmail.com>
2018-08-14 17:36:03 +0200

    d850: set matching build fingerprint for system, vendor and boot images
    This makes all build fingerprints match on system, vendor and boot and fixes vendor image mismatch. (https://i.imgur.com/EGEnKqj.jpg)
    Needed after https://review.lineageos.org/#/c/208868
    
    Change-Id: I555c15eddc6dff694285ab1b23f6f9adfd64e535

--- lineage.mk
+++ lineage.mk
@@ -8,5 +8,6 @@ PRODUCT_NAME := lineage_d850
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_att_us" \
-    BUILD_FINGERPRINT="lge/g3_att_us/g3:4.4.2/KVT49L.D85010d/D85010d.1403173065:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_att_us-user 4.4.2 KVT49L.D85010d D85010d.1403173065 release-keys"
+
+BUILD_FINGERPRINT="lge/g3_att_us/g3:4.4.2/KVT49L.D85010d/D85010d.1403173065:user/release-keys"
