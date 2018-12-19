9b84ee997547e8d147831a9e051570e36fd00efb
Nolen Johnson <johnsonnolen@gmail.com>
2018-12-19 23:15:00 +0100

    d850: Migrate from system.prop to system_prop.mk
     * This is the new way to do things as of AOSP 8.0.
     * This will allow us to split system/vendor properties if we want to.
    
    Change-Id: Ia147eb39342122bf0ecdcd3f2d3c2338569fc2da

---
+++ system_prop.mk
@@ -0,0 +1,10 @@
+#
+# System properties for d850
+#
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
+    ro.ril.telephony.mqanelements=4 \
+    ro.telephony.default_network=9 \
+    telephony.lteOnCdmaDevice=1
