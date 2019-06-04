d771c49ff6c39368cf61e80184a6839595688c87
Aleksander Gencel <johnnylittleplanet@gmail.com>
2019-06-04 15:50:45 +0200

    d850: Add bluetooth properties
     * The first property is used for FM2 and JNI
     * Add the second property too, better safe than sorry
    
    Change-Id: Ib1cad8e05ece3d1700648776e6ca05af82695a15

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,11 @@
 # System properties for d850
 #
 
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.bluetooth.soc=smd \
+    vendor.qcom.bluetooth.soc=smd
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
